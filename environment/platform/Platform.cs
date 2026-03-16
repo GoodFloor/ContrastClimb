ff944c78a449226d5d0d5c66cd6bb810c549abe7
Łukasz Machnik <49206646+GoodFloor@users.noreply.github.com>
2026-03-16 19:43:41 +0100

    exported platform's color property

--- environment/platform/Platform.cs
+++ environment/platform/Platform.cs
@@ -3,6 +3,15 @@ using System;
 
 public partial class Platform : StaticBody2D
 {
+    [Export] 
+    private bool _isWhite;
+
+    public override void _Ready()
+    {
+        if (_isWhite)
+            Visible = false;
+    }
+    
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
