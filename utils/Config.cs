ee7689962ba6314969be2b1421c670352bdda87c
Łukasz Machnik <49206646+GoodFloor@users.noreply.github.com>
2026-03-24 19:41:02 +0100

    Supplement missing settings

--- utils/Config.cs
+++ utils/Config.cs
@@ -6,26 +6,38 @@ public partial class Config : GodotObject
 {
     private string _configPath = "res://config.cfg";
     private ConfigFile _configFile = new ConfigFile();
-    public MovementType Steering;
+    public MovementType Steering
+    {
+        get => (MovementType)(int)_configFile.GetValue("gameplay", "steering");
+        set
+        {
+            _configFile.SetValue("gameplay", "steering", (int)value);
+            _configFile.Save(_configPath);
+        } 
+    }
     
     public void LoadConfig()
     {
-        GD.Print("LoadConfig");
-        if (_configFile.Load(_configPath) != Error.Ok)
-            GenerateDefaultConfig();
-
-        Steering = (MovementType)(int)_configFile.GetValue("gameplay", "steering");
+        _configFile.Load(_configPath);
+        
+        GenerateDefaultConfig();
     }
-
-
+    
+    /// <summary>
+    /// Checks if all the expected config options are present, if a definition is not present, then it sets a default value for it.
+    /// </summary>
     private void GenerateDefaultConfig()
     {
-        // Default steering on mobile device is with tilting
-        _configFile.SetValue("gameplay", "steering", (int)MovementType.Tilt);
+        if (!_configFile.HasSectionKey("gameplay", "steering"))
+        {
+            // Default steering on mobile device is with tilting
+            _configFile.SetValue("gameplay", "steering", (int)MovementType.Tilt);
         
-        // If the device doesn't have accelerometer, then it defaults to dragging
-        if (Input.GetGravity() == Vector3.Zero)
-            _configFile.SetValue("gameplay", "steering", (int)MovementType.Drag);
+            // If the device doesn't have accelerometer, then it defaults to dragging
+            if (Input.GetGravity() == Vector3.Zero)
+                _configFile.SetValue("gameplay", "steering", (int)MovementType.Drag);
+        }
+        
         
         _configFile.Save(_configPath);
     }
