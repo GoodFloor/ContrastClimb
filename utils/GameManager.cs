19fb1cde0e0fac6b8140f66a54943a57ca5137ce
Łukasz Machnik <49206646+GoodFloor@users.noreply.github.com>
2026-03-24 11:04:11 +0100

    Added a game manager

---
+++ utils/GameManager.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class GameManager : Node
+{
+    private Node2D _levelRoot;
+    private PackedScene _currentLoadedLevel;
+    private Node2D _currentInstanceLevel;
+    public override void _Ready()
+    {
+        Global.GameManager = this;
+        _levelRoot = GetNode<Node2D>("LevelRoot");
+        _currentLoadedLevel = ResourceLoader.Load<PackedScene>("res://levels/demo_level.tscn");
+        _currentInstanceLevel = _currentLoadedLevel.Instantiate<Node2D>();
+        _levelRoot.AddChild(_currentInstanceLevel);
+    }
+}
