e012ffcb3bff6137ea20af848ace8a1bf8d2582d
Łukasz Machnik <49206646+GoodFloor@users.noreply.github.com>
2026-04-06 18:58:55 +0200

    Moved resume and pause logic to game manager

--- utils/GameManager.cs
+++ utils/GameManager.cs
@@ -6,6 +6,8 @@ public partial class GameManager : Node
     private Node2D _levelRoot;
     private PackedScene _currentLoadedLevel;
     private Node2D _currentInstanceLevel;
+
+    private Control _uiRoot;
     
     public override void _Ready()
     {
@@ -16,11 +18,23 @@ public partial class GameManager : Node
         Global.Config.LoadConfig();
         
         _levelRoot = GetNode<Node2D>("LevelRoot");
-        
+        _uiRoot = GetNode<Control>("UIRoot");
         
         _currentLoadedLevel = ResourceLoader.Load<PackedScene>("res://levels/demo_level.tscn");
         _currentInstanceLevel = _currentLoadedLevel.Instantiate<Node2D>();
         _levelRoot.AddChild(_currentInstanceLevel);
         
+    }
+
+    public void PauseGame()
+    {
+        GetTree().Paused = true;
+        _uiRoot.Visible = true;
+    }
+
+    public void ResumeGame()
+    {
+        GetTree().Paused = false;
+        _uiRoot.Visible = false;
     }
 }
--- utils/ui/ContinueButton.cs
+++ utils/ui/ContinueButton.cs
@@ -6,6 +6,7 @@ public partial class ContinueButton : Button
     public override void _Pressed()
     {
         base._Pressed();
-        GetTree().Paused = false;
+        
+        Global.GameManager.ResumeGame();
     }
 }
