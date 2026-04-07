c440e841e39b67f7d9b25c6f04d8e5cb911dd18d
Łukasz Machnik <49206646+GoodFloor@users.noreply.github.com>
2026-04-07 23:23:20 +0200

    Moving camera

---
+++ levels/DemoLevel.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class DemoLevel : Node2D
+{
+    private Path2D _pathCamera;
+    private PathFollow2D _pathFollow;
+    private float _speed = 0.1f;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        _pathCamera = GetNode<Path2D>("PathCamera");
+        _pathFollow = _pathCamera.GetNode<PathFollow2D>("PathFollowCamera");
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        float df = (float)delta;
+        
+        _pathFollow.ProgressRatio += df * _speed;
+    }
+}
