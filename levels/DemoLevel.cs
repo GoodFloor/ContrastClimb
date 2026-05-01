64d1f6bc0c447358d0a46b68d81f018db2be9b21
Łukasz Machnik <49206646+GoodFloor@users.noreply.github.com>
2026-05-01 22:21:02 +0200

    Camera catching up with the player

--- levels/DemoLevel.cs
+++ levels/DemoLevel.cs
@@ -3,29 +3,22 @@ using System;
 
 public partial class DemoLevel : Node2D
 {
-    private Path2D _pathCamera;
-    private PathFollow2D _pathFollow;
-    private float _speed = 0.1f;
-    private bool _isCameraStarted = false;
+    private CameraPath _cameraPath;
 
     public override void _Ready()
     {
         base._Ready();
-        _pathCamera = GetNode<Path2D>("PathCamera");
-        _pathFollow = _pathCamera.GetNode<PathFollow2D>("PathFollowCamera");
+        _cameraPath = GetNode<CameraPath>("CameraPath");
     }
 
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
         float df = (float)delta;
-
-        if (_isCameraStarted)
-            _pathFollow.ProgressRatio += df * _speed;
     }
 
     public void StartCamera()
     {
-        _isCameraStarted = true;
+        _cameraPath.StartCamera();
     }
 }
---
+++ utils/camera/CameraPath.cs
@@ -0,0 +1,47 @@
+using Godot;
+
+public partial class CameraPath : Path2D
+{
+    private PathFollow2D _pathFollow;
+    
+    private const float DefaultSpeed = 0.1f;
+    private const float SpeedModifier = 0.1f;
+    private float _speed = DefaultSpeed;
+    public override void _Ready()
+    {
+        base._Ready();
+        SetPhysicsProcess(false);
+        _pathFollow = GetNode<PathFollow2D>("PathFollow");
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        float df = (float)delta;
+        
+        _pathFollow.ProgressRatio += df * _speed;
+    }
+
+    public void StartCamera()
+    {
+        SetPhysicsProcess(true);
+    }
+
+    private void OnBodyEnteredSpeedIncrease(Node2D body)
+    {
+        if (body is not Player)
+            return;
+        
+        _speed += SpeedModifier;
+    }
+
+    private void OnBodyEnteredSpeedDecrease(Node2D body)
+    {
+        if (body is not Player)
+            return;
+        
+        if (_speed > DefaultSpeed)
+            _speed -= SpeedModifier;
+    }
+    
+}
