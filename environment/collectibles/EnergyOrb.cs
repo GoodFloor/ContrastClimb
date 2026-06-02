ed8066a468c953b852ef570a852ae6653aee1164
Łukasz Machnik <49206646+GoodFloor@users.noreply.github.com>
2026-06-02 22:31:30 +0200

    Energy orb animated sprite

--- environment/collectibles/EnergyOrb.cs
+++ environment/collectibles/EnergyOrb.cs
@@ -6,6 +6,8 @@ namespace ContrastClimb.environment.collectibles;
 
 public partial class EnergyOrb : Area2D
 {
+    private bool _currentlyWhite;
+    
     private void OnBodyEntered(Node2D body)
     {
         if (body is not Player)
@@ -13,5 +15,27 @@ public partial class EnergyOrb : Area2D
         
         Global.GameManager.EnergyLeftOverlay.AddEnergy();
         QueueFree();
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        Global.GameManager.Connect(
+            GameManager.SignalName.ColorChanged,
+            new Callable(this, MethodName.OnColorChanged)
+        );
+
+        Modulate = GColors.Dark;
+        _currentlyWhite = false;
+        
+        GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("idle");
+    }
+
+    private void OnColorChanged()
+    {
+        _currentlyWhite = !_currentlyWhite;
+
+        Modulate = _currentlyWhite ? GColors.Light : GColors.Dark;
     }
 }
